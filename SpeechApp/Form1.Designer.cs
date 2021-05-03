7a74591195fd2e85c38e0a6c44e52d0496dddfa7
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2021-05-03 17:12:26 -0300

    Voz a texto

---
+++ SpeechApp/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SpeechApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TmrSpeaking = new System.Windows.Forms.Timer(this.components);
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // TmrSpeaking
+            // 
+            this.TmrSpeaking.Enabled = true;
+            this.TmrSpeaking.Interval = 1000;
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(75, 137);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.TabIndex = 0;
+            this.btnSwitch.Text = "Activar";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Location = new System.Drawing.Point(94, 54);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(0, 13);
+            this.lblTexto.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 196);
+            this.Controls.Add(this.lblTexto);
+            this.Controls.Add(this.btnSwitch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TmrSpeaking;
+        private System.Windows.Forms.Button btnSwitch;
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ SpeechApp/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace SpeechApp
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine reconocedor = new SpeechRecognitionEngine();
+
+        int RecTimeOut = 0;
+        bool escuchando = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void reconocedor_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void Default_SpeechRecognizer(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            this.lblTexto.Text = speech;
+        }
+
+        private void TmrSpeaking_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                reconocedor.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 10)
+            {
+                TmrSpeaking.Stop();
+                RecTimeOut = 0;
+            }
+        }
+
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            if (!escuchando)
+            {
+                reconocedor.RecognizeAsync(RecognizeMode.Multiple);
+                this.btnSwitch.Text = "Desactivar";
+                escuchando = true;
+
+            }
+            else
+            {
+                reconocedor.RecognizeAsyncCancel();
+                this.btnSwitch.Text = "Activar";
+                this.lblTexto.Text = "";
+                escuchando = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reconocedor.SetInputToDefaultAudioDevice();
+            reconocedor.LoadGrammarAsync(new DictationGrammar());
+            reconocedor.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognizer);
+            reconocedor.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(reconocedor_SpeechRecognized);
+            //reconocedor.RecognizeAsync(RecognizeMode.Multiple);
+        }
+    }
+}
